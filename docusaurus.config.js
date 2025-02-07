ecd10ff703e4d306bd784fd92d4aaab1d5aee228
Maurice Handwerker <maurice.kau@outlook.com>
2025-02-07 01:13:31 +0100

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,7 +15,7 @@ const config = {
   favicon: 'img/logo.ico',
 
   // Set the production url of your site here
-  url: 'https://schurkenliga.github.io/',
+  url: '/Public-API-Documentaion/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
